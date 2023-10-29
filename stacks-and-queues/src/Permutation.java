b92848468e0c40feaac03fa549d22aec5f7914bb
michael kan <michael.kan.mail@gmail.com>
2023-10-29 19:57:19 +0100

    add link

--- stacks-and-queues/src/Permutation.java
+++ stacks-and-queues/src/Permutation.java
@@ -1,5 +1,5 @@
 import edu.princeton.cs.algs4.StdIn;
-
+//https://www.coursera.org/learn/algorithms-part1/programming/zamjZ/deques-and-randomized-queues
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
